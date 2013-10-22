49a4325ed1eb8817c19dbb969075239831239536
jay-menorca <zai_menorca@yahoo.com>
2013-10-22 14:04:22 -0400

    commiting exer 2 answers

--- exercise2.rb
+++ exercise2.rb
@@ -1,12 +1,17 @@
-=begin 
+#Exercise 2 : Jay Menorca
+
+
+class TipCalculator
+
+
+=begin --------------------------------------------------------------
+
+Exercise 2 : Question 1
 
 How would you calculate a good tip for a 55 dollar meal? 
 Use puts to print the answer onscreen.
 	
 =end
-
-class TipCalculator
-
 
 	def calculateTip(amt)
 		amt = amt * 0.15;
@@ -20,5 +25,51 @@ class TipCalculator
 
 	tip = tipCalc.calculateTip(amt)
 
-	puts "recommended tip is: " + tip
+	puts "recommended tip is: #{tip}"
+
+
+=begin -------------------------------------------------------------- 
+
+Exercise 2 : Question 2
+
+Try adding a string and an integer. What happens? 
+Find a way to convert the integer so that it works and use puts to 
+print the answer onscreen.
+
+Jay's Answer:
+From the top of my head, what happens when you try to add a string and an 
+integer is you'd have an exception. To make this work, you can use a 'wrapper' 
+around the variable so the + operator can work
+	
+=end
+
+=begin -------------------------------------------------------------- 
+
+Exercise 2 : Question 3
+
+Evidently, Ruby is much more than just a calculator, but try outputting 
+the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+	
+=end
+
+	puts "45628 x 7839 = #{45628 * 7839}"
+
+
+=begin -------------------------------------------------------------- 
+
+Exercise 2 : Question 4
+
+What's the value of the expression 
+(true && false) || (false && true) || !(false && false)? 
+Try figuring it out on your own before typing it in.
+	
+=end
+
+
+#Jay's answer = false
+puts "What's the value of the expression:\n" +  
+"(true && false) || (false && true) || !(false && false)? =  
+#{(true && false) || (false && true) || !(false && false)}"
+
+
 end
