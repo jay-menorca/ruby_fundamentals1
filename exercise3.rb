affc927cf8000cd81b3763bb0c23b43620f402f0
jay-menorca <zai_menorca@yahoo.com>
2013-10-22 14:57:21 -0400

    commiting exercise3.rb

---
+++ exercise3.rb
@@ -0,0 +1,22 @@
+#Exercise 2 : Jay Menorca
+
+
+class FriendlyGuyImpl
+
+	@@buddyName = "buddy"
+
+	def askName
+		puts "What's your name?"
+		@@buddyName = gets.chomp
+	end
+
+	def greetBack
+		puts "hi there #@@buddyName!"
+	end
+
+	regularGuy = FriendlyGuyImpl.new
+#	regularGuy = new.FriendlyGuyImpl("buddy")
+	regularGuy.askName
+	regularGuy.greetBack
+
+end
